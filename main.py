0886ec8d4966042e034320a1d3867dda2343a52f
Satish Vudaru <satishvudaru@gmail.com>
2026-04-08 13:35:42 -0500

    add layout pages and new routes

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ templates = Jinja2Templates (directory="templates")
 posts: list[dict] = [
     {"id": 1, "title": "First Post", "content": "This is the first post."},
     {"id": 2, "title": "Second Post", "content": "This is the second post."},
-    {"id": 3, "title"   : "Third Post", "content": "This is the third post."},
+    {"id": 3, "title": "Third Post", "content": "This is the third post."},
 ]
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
-
+    return templates.TemplateResponse( request=request,
+        name="home.html" , context={"posts":posts,"title":"Awesome application!"}
+        
+    )
  
+@app.get("/nice")
 def nicerhome():
     return "<h1>Fast api is awesome</h1>"
 
